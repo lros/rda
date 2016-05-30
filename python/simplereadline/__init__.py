7cc47368b21ed3c2f85be4e870ec03bb23c9a205
Steven J. Clark <sclark724@gmail.com>
2016-05-30 15:03:48 -0400

    Fixing case insensitivity debris.

--- python/simplereadline/__init__.py
+++
@@ -1,6 +0,0 @@
-# Simplified version of Python readline module that does not require the
-# GNU readline package (which is available only on Linux).
-
-from simplereadline.interpretKeys import configure
-from simplereadline.interpretKeys import addByte
-
